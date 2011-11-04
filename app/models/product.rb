4227198af8ddb4097b3ba8cde183ced56c6fdc9a
Tyrone Gaylord <tyrone.mdd@gmail.com>
2011-11-04 16:55:10 -0500

    created store index page for shoping cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,16 @@
 class Product < ActiveRecord::Base
+  default_scope :order => 'title'
+  
+  validates_presence_of :title
+  validates_presence_of :description
+  validates_presence_of :image_url
+  # validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates_numericality_of :price, :greater_than_or_equal_to => 0.01
+  validates_uniqueness_of :title
+  validates :image_url, :format => {
+  :with => %r{\.(gif|jpg|png)$}i,
+  :message => 'must be a URL for GIF, JPG or PNG image.'
+  }
+
+  
 end
